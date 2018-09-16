95c0e4d3926c2c67326722277d616e0e30cf1ec6
Hugo Quiñonez <hugo_2555@hotmail.com>
2018-09-16 17:39:39 -0600

    session

--- ThunderOnlineModule/Controllers/HomeController.cs
+++ ThunderOnlineModule/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ThunderOnlineModule.Models;
 
 namespace ThunderOnlineModule.Controllers
 {
@@ -17,7 +20,25 @@ namespace ThunderOnlineModule.Controllers
             }
             else
             {
-                ViewBag.UserName =  System.Web.HttpContext.Current.Session["logged"].ToString();
+                string connetionString = null;
+                SqlConnection cnn;
+                connetionString = "Data Source=TCESbase.db.10029297.hostedresource.com;Initial Catalog=TCESbase;User ID=TCESbase;Password=Salmo119@165";
+                cnn = new SqlConnection(connetionString);
+
+                SqlCommand testCMD = new SqlCommand("sp_spaceused", cnn);
+                testCMD.CommandType = CommandType.StoredProcedure; cnn.Open();
+
+                SqlDataReader reader = testCMD.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var msg = reader["database_size"];
+                        ViewBag.SpaceUsed = msg;
+                    }
+                }
+                ViewBag.UserName = System.Web.HttpContext.Current.Session["logged"].ToString();
             }
 
             return View();
@@ -25,6 +46,9 @@ namespace ThunderOnlineModule.Controllers
 
         public ActionResult Hugo()
         {
+           
+
+
             return View();
         }
 
